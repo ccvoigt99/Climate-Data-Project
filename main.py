747ed4781d1200675555ad66f994a59f3a7e809d
Mitch Phillipson <mitch.phillipson@gmail.com>
2025-06-09 11:04:12 -0500

    One more graphing example

--- main.py
+++ main.py
@@ -45,3 +45,34 @@ ax.set_title("Total Number of 90 Degree Days by Location, 2014-2024")
 
 # %%
 
+def number_90_days_lineplot(df, city, ax):
+    sns.lineplot(
+        data = df.query(f"CITY == '{city}'"),
+        x = "YEAR",
+        y = "DailyMaximumDryBulbTemperature",
+        ax = ax
+    )
+
+    ax.set_title(f"Number of 90 Degree Days Per Year: {city}")
+                
+
+X = (df
+        .query("DailyMaximumDryBulbTemperature >= 90")
+        .assign(YEAR=df.DATE.dt.year)
+        .groupby(["CITY", "YEAR"])
+        .agg({"DailyMaximumDryBulbTemperature": "count"})
+        .reset_index()
+)
+    
+
+fig, ax = plt.subplots(2,2, figsize=(12, 12), sharey=True, sharex=True) 
+cities = ['Madison, WI', 'Rapid City, SD', 'Jackson, MS', 'Albuquerque, NM']
+
+number_90_days_lineplot(X, cities[0], ax[0,0])
+number_90_days_lineplot(X, cities[1], ax[0,1])
+number_90_days_lineplot(X, cities[2], ax[1,0])
+number_90_days_lineplot(X, cities[3], ax[1,1])
+
+
+X
+# %%
